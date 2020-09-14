499982379d4590bbbaba4bdeff04b9e60a5f21df
myselfjyoti93 <myselfjyoti93@gmail.com>
2020-09-14 22:12:00 +0530

    Removing unwanted changes

--- DehatFlipkart/SalesAgent.py
+++ DehatFlipkart/SalesAgent.py
@@ -10,21 +10,3 @@ class SalesAgent(Account):
       self.logger.ERROR("Not an Sales Agent")
       return
     super().__init__(user_name, password, role)
-
-  def create_users_for_roles(self, name, role):
-    None
-
-  def add_parking_spot(self, floor_name, spot):
-    None
-
-  def add_parking_display_board(self, floor_name, display_board):
-    None
-
-  def add_customer_info_panel(self, floor_name, info_panel):
-    None
-
-  def add_entrance_panel(self, entrance_panel):
-    None
-
-  def add_exit_panel(self, exit_panel):
-    None
